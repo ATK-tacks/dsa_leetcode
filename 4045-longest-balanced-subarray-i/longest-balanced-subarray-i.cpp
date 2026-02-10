747a68a9c5bb13a2849feea65f4e158983c9cf65
ATK <atirkhanjmi@gmail.com>
2026-02-10 17:41:32 +0530

    Time: 1753 ms (80.09%) | Memory: 519.3 MB (57.44%) - LeetSync

---
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+
+        for (int l = 0; l < n; l++) {
+            unordered_set<int> even, odd;
+
+            for (int r = l; r < n; r++) {
+                if (nums[r] % 2 == 0)
+                    even.insert(nums[r]);
+                else
+                    odd.insert(nums[r]);
+
+                if (even.size() == odd.size()) {
+                    ans = max(ans, r - l + 1);
+                }
+
+                // 🔥 pruning: cannot recover balance
+                if (abs((int)even.size() - (int)odd.size()) > (n - r))
+                    break;
+            }
+        }
+        return ans;
+    }
+};
